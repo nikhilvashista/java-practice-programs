30d1ffb6ebc694c14a004ec52c01da7113c648a9
nikhilvashista <b.nikhilvashista@gmail.com>
2021-11-27 10:53:32 +0530

    Initial Commit

--- practice/src/Demo1.java
+++ practice/src/Demo1.java
@@ -1,7 +1,7 @@
 public class Demo1 {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to java world World!");
+        System.out.println("Welcome to java World!");
     }
 
 }
