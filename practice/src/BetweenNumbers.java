d38494866f9a48f071fc5a83e381eb735a29dcc7
nikhilvashista <b.nikhilvashista@gmail.com>
2021-11-27 11:11:27 +0530

    Program to find numbers between a given range

---
+++ practice/src/BetweenNumbers.java
@@ -0,0 +1,16 @@
+public class BetweenNumbers {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+
+        for (int i=a+1; i<b; i++)
+         {
+
+             System.out.println(i);
+
+         }
+
+    }
+}
